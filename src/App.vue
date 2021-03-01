beec31f159cf448700630b8e8ff9a8888dd6c98d
svtslv17 <svtslv17@gmail.com>
2021-03-02 00:09:48 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+    <component :is="layout">
+      <router-view />
+    </component>
+  </div>
+</template>
+
+<script>
+import MainLayout from "@/layouts/MainLayout";
+
+import EmptyLayout from "@/layouts/EmptyLayout";
+
+export default {
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || "empty") + "-layout";
+    },
+  },
+  components: {
+    EmptyLayout,
+    MainLayout,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../node_modules/materialize-css/dist/css/materialize.min.css";
+@import "assets/index.css";
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,65 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    meta: { layout: "main" },
+    component: () => import("../views/Home.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    meta: { layout: "empty" },
+    component: () => import("../views/Login.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    meta: { layout: "empty" },
+    component: () => import("../views/Register.vue"),
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    meta: { layout: "main" },
+    component: () => import("../views/Categories.vue"),
+  },
+  {
+    path: "/detail-record",
+    name: "detail-record",
+    meta: { layout: "main" },
+    component: () => import("../views/Detail-Record.vue"),
+  },
+  {
+    path: "/history",
+    name: "history",
+    meta: { layout: "main" },
+    component: () => import("../views/History.vue"),
+  },
+  {
+    path: "/planning",
+    name: "planning",
+    meta: { layout: "main" },
+    component: () => import("../views/Planning.vue"),
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    meta: { layout: "main" },
+    component: () => import("../views/Profile.vue"),
+  },
+  {
+    path: "/record",
+    name: "record",
+    meta: { layout: "main" },
+    component: () => import("../views/Record.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
